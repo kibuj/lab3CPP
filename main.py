a94f9e59328480e18443a20943f9053029afc902
bogdan <bogdankibuj@gmail.com>
2025-03-14 15:23:50 +0200

    Step 3

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+from functools import reduce
 
-
+#Рекурсивно
 def factorial_rec(n):
     if n == 0:
         return 1
@@ -14,6 +15,7 @@ def fibonacci_rec(n):
     else:
         return fibonacci_rec(n - 1) + fibonacci_rec(n-2)
 
+#Циклічно
 def factorial_loop(n):
     fact = 1
     for i in range(1,n + 1):
@@ -27,9 +29,27 @@ def fibonacci_loop(n):
         a, b = b, a + b
     return b
 
+#reduce
+def factorial_reduce(n):
+    if n == 0:
+        return 1
+    else:
+        return reduce(lambda x,y: x * y,range(1, n + 1))
 
-ex = 5
+def fibonacci_reduce(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    s =  reduce(lambda x, _: (x[1], x[0] + x[1]), range(n - 1), (0, 1))
+    return s[1]
+
+
+
+ex = 10
 print(f"Факторіал рекурсивно = {factorial_rec(ex)},\n"
       f"Фібоначчі рекурсивно = {fibonacci_rec(ex)},\n"
       f"Факторіал циклом = {factorial_loop(ex)},\n"
-      f"Фібоначчі циклом = {fibonacci_loop(ex)}")
+      f"Фібоначчі циклом = {fibonacci_loop(ex)},\n"
+      f"Факторіал reduce = {factorial_reduce(ex)},\n"
+      f"Фібоначчі reduce = {fibonacci_reduce(ex)}")
